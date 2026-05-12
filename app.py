e4ec98ec2b1564d1316b2fe59e6339f9282a72c9
Nicolaorhjaijf <ncaecli@upv.edu.es>
2026-05-12 18:28:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.write("Dibuja un número del 0 al 9 en el recuadro negro.")
 # 1. Cargar el modelo guardado
 @st.cache_resource
 def load_my_model():
-    return tf.keras.models.load_model('modelo_mnist.keras')
+    return tf.keras.models.load_model('modelo_mnist.keras')  # <--- ESTA LÍNEA DEBE TENER ESPACIOS AL PRINCIPIO
 
 model = load_my_model()
 
