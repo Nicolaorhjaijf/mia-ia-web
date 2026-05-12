4f35acf7bf046f8a0af83ccdee4e44ee74cca9cd
Nicolaorhjaijf <ncaecli@upv.edu.es>
2026-05-12 18:24:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-import streamlit as st
-import tensorflow as tf
-from streamlit_drawable_canvas import st_canvas
-import cv2
-import numpy as np
+import streamlit as st 
+import tensorflow as tf 
+from streamlit_drawable_canvas import st_canvas [cite: 44]
+import cv2 
+import numpy as np 
+
+# Configuración de la página
+st.set_page_config(page_title="IA Digit Recognizer") 
+st.title("Reconocedor de Digitos en Tiempo Real") 
